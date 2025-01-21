ad6922ab457e9b47e47baebd662a93f3bba8487c
Jad-Doggy <pandajadon@gmail.com>
2025-01-20 17:04:52 -0800

    making sure the code compiles

--- Assets/Scripts/BattleSim.cs
+++ Assets/Scripts/BattleSim.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Unity.VisualScripting;
 using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
@@ -10,6 +12,9 @@ public class BattleSim : MonoBehaviour
 
     private Pookiemon playerOneActive;
     private Pookiemon playerTwoActive;
+
+    private bool playerOneIsDead;
+    private bool playerTwoIsDead;
 
     private int turnNumber;
 
@@ -28,7 +33,8 @@ public class BattleSim : MonoBehaviour
         playerOneActive = teamOne[0];
         playerTwoActive = teamTwo[0];
 
-        
+        playerOneIsDead = false;
+        playerTwoIsDead = false;
     }
 
     private void SimulateBattle() // battle loop
@@ -54,7 +60,37 @@ public class BattleSim : MonoBehaviour
 
     private void TurnUpKeep()
     {
-        
+        if (CheckTeamIsDead(teamOne) ||  CheckTeamIsDead(teamTwo)) // First check to see if teams are alive. End the game if not
+        {
+            if (playerOneIsDead)
+            {
+                Debug.Log("Player 2 Wins!");
+            } else if (playerTwoIsDead)
+            {
+                Debug.Log("Player 1 Wins!");
+            }
+            EndBattle();
+        }
+        FieldEffect(); // Updates any field effects
+    }
+
+    private bool CheckTeamIsDead(List<Pookiemon> playerTeam)
+    {
+        foreach (Pookiemon p in playerTeam)
+        {
+            if (!p.IsDead)
+                return false;
+        }
+
+        if (playerTeam == teamOne)
+        {
+            playerOneIsDead = true;
+        } else
+        {
+            playerTwoIsDead = true;
+        }
+
+        return true;
     }
 
     private void FieldEffect() // evaluates every on field affect
